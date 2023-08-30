0b691f8c06618da03433d60c7eb146666489c20d
J7 <j7.dev.gg@outlook.com>
2023-08-31 06:36:59 +0800

    feat: merge i18n

---
+++ src/i18n/resources.ts
@@ -0,0 +1,21 @@
+import en from './language/en.json';
+import cn from './language/zh-CN.json';
+import tw from './language/zh-TW.json';
+import ko from './language/ko.json';
+
+const resources = {
+    en: {
+        translation: en,
+    },
+    cn: {
+        translation: cn,
+    },
+    tw: {
+        translation: tw,
+    },
+    ko: {
+        translation: ko,
+    },
+};
+
+export default resources;
