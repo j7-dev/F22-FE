e5aea85ddcf530e71850d18c67090f1d6f10fc88
J7 <j7.dev.gg@outlook.com>
2023-08-31 11:42:04 +0800

    fix: add images to build.rollupOptions.external

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths()],
+    build: {
+        rollupOptions: {
+            external: [/^@\/assets\/images\/.*\.(jpg|png|jpeg|gif)$/],
+        },
+    },
 });
