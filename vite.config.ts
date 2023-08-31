c3d64aab1db2adde203a815ba727f05e1c248b75
J7 <j7.dev.gg@outlook.com>
2023-08-31 12:05:35 +0800

    fix: vite config ,add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import * as path from 'path';
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    build: {
-        rollupOptions: {
-            external: [/^.\/src\/\/assets\/images\/.*\.(jpg|png|jpeg|gif)$/],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
         },
     },
+    plugins: [react(), tsconfigPaths()],
+    // build: {
+    //     rollupOptions: {
+    //         external: [/^@\/assets\/images\/.*\.(jpg|png|jpeg|gif)$/],
+    //     },
+    // },
 });
