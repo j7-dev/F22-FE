d4c1c56693ec25b2871c83f49c413a6c5a45ac61
Ren <s0985514623@gmail.com>
2023-09-01 16:44:05 +0800

    fix:SubMenu import MenuItem error

--- src/components/Layout/Header/NavBar/SubMenu/index.tsx
+++ src/components/Layout/Header/NavBar/SubMenu/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 import { Link } from 'react-router-dom';
-import { MenuItem } from '@/components/Layout/Header/NavBar/index';
+import { MenuItem } from '@/components/Layout/Header/MenuData';
 
 const SubMenu: React.FC<{ submenu: MenuItem[] }> = ({ submenu }) => (
     <>
