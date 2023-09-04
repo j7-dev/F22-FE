dd7e981341932b76537ab6acb41cd7f39a190357
Ren <s0985514623@gmail.com>
2023-09-05 02:43:35 +0800

    fix:ICON調整

--- src/components/ContentLayout/Header/NavBar/index.tsx
+++ src/components/ContentLayout/Header/NavBar/index.tsx
@@ -148,16 +148,7 @@ const NavBar: React.FC = () => {
                             >
                                 <Link to="/wallet">
                                     <span className="flex whitespace-nowrap  items-center rounded-lg border-white gap-x-2 font-bold bg-[#F9A318] text-white hover:opacity-80  md:my-3 md:px-6 md:py-3 ">
-                                        <svg
-                                            className="w-4 h-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="16"
-                                            height="16"
-                                            fill="currentColor"
-                                            viewBox="0 0 16 16"
-                                        >
-                                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                                        </svg>
+                                        <BiSolidUser size={20} />
                                         My Wallet
                                     </span>
                                 </Link>
