a9ec4b24f31f040f8ce153ed4530ead567380138
J7 <j7.dev.gg@outlook.com>
2023-09-05 20:43:47 +0800

    feat: image compress

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 
@@ -11,5 +12,35 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 100,
+            },
+            pngquant: {
+                quality: [0.8, 1],
+                speed: 2,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
 });
