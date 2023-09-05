5acb935c1e5b2da02799cb7d1a7fa9325b0bbf27
J7 <j7.dev.gg@outlook.com>
2023-09-05 09:16:01 +0800

    WIP: ABOUT手機板

--- src/pages/Content/About/index.tsx
+++ src/pages/Content/About/index.tsx
@@ -25,196 +25,104 @@ const About: React.FC = () => {
         <>
             <div className="aboutSection w-full h-[500px] flex justify-center items-center">
                 <div
-                    className="mainContainer h-full w-full flex justify-center items-center"
+                    className="mainContainer h-full w-full flex justify-center items-center overflow-hidden bg-no-repeat bg-cover"
                     style={{
                         backgroundImage: `url(${bgImg})`,
                     }}
                 >
-                    <img
-                        className="absolute top-10 left-60"
-                        src={slot}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-28 left-12"
-                        src={emoji}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-16 left-80"
-                        src={bubble}
-                        alt=""
-                    />
-                    <div className="title flex flex-col justify-center items-center gap-5">
-                        <img src={smartplayEn} alt="" />
-                        <span className="bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl">
-                            {t('Learn the standards of responsible play!')}
-                        </span>
-                        <button className="bg-[#4BA1FA] border-0 rounded-lg w-[145px] h-10 flex justify-center items-center text-white font-bold text-sm">
-                            {t('Inquiry')}
-                        </button>
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[20%] left-[16%] -ml-[60px] -mt-[60px]" src={slot} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[60%] left-[7%] -ml-[83px] -mt-[83px]" src={emoji} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[80%] left-[19%] -ml-[33px] -mt-[34px]" src={bubble} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[19%] left-[83%] -ml-[33px] -mt-[34px]" src={bubble} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[40%] left-[93%] -ml-[70px] -mt-[88px]" src={card} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[80%] left-[81%] -ml-[113px] -mt-[60px]" src={roulette} />
+                    <div className="title flex flex-col justify-center items-center gap-5 mb-12 z-90 relative">
+                        <img src={smartplayEn} />
+                        <span className="text-center px-8 bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl">{t('Learn the standards of responsible play!')}</span>
+                        <button className="bg-[#4BA1FA] border-0 rounded-lg w-[145px] h-10 flex justify-center items-center text-white font-bold text-sm">{t('Inquiry')}</button>
                     </div>
-                    <img
-                        className="absolute top-16 right-72"
-                        src={bubble}
-                        alt=""
-                    />
-                    <img
-                        className="absolute top-28 right-16"
-                        src={card}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-9 right-52"
-                        src={roulette}
-                        alt=""
-                    />
                 </div>
             </div>
             <div className="responsibleSection-2">
-                <div className="mainContainer p-16 h-full w-full flex flex-col gap-5 justify-center items-center bg-[#FAFAFA]">
-                    <img
-                        className="absolute bottom-0 left-0"
-                        src={girl1}
-                        alt=""
-                    />
-                    <div className="list-1 w-[750px] flex flex-col justify-center items-center gap-5">
-                        <div className="headerTitle-1  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl whitespace-nowrap">
-                            {t(
-                                'Becoming deeply involved in gambling can cause various problems',
-                            )}
-                        </div>
+                <div className="mainContainer py-16 px-8 md:px-16 h-full w-full flex flex-col gap-5 justify-center items-center bg-[#FAFAFA] overflow-hidden">
+                    <img className="scale-50 md:scale-100 z-0 absolute -bottom-[134px] md:bottom-0 left-[8%] -ml-[150px]" src={girl1} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[22%] left-[92%] -ml-[91px] -mt-[70px]" src={card1} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[77%] left-[89%] -ml-[79px] -mt-[60px]" src={dice} />
+                    <div className="list-1 w-full md:w-[750px] flex flex-col justify-center items-center gap-5 mb-12">
+                        <div className="headerTitle-1  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl text-center">{t('Becoming deeply involved in gambling can cause various problems')}</div>
                         <div className="contentList-1">
-                            <ul className="list-disc gap-2.5 flex flex-col my-0">
-                                <li className="list-disc text-lg font-bold text-[#2B3240]">
+                            <ul className="list-disc gap-2.5 flex flex-col m-0 p-0">
+                                <li className="list-disc text-lg font-normal text-[#2B3240]">
                                     <Trans i18nKey="Becoming Content" />
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="list-2 w-[750px] flex flex-col justify-center items-center gap-5">
-                        <div className="headerTitle-2  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl whitespace-nowrap">
-                            {t(
-                                'Gambling addiction is a risk that anyone can fall into',
-                            )}
-                        </div>
+                    <div className="list-2 w-full md:w-[750px] flex flex-col justify-center items-center gap-5 mb-12">
+                        <div className="headerTitle-2  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl text-center">{t('Gambling addiction is a risk that anyone can fall into')}</div>
                         <div className="contentList-2">
-                            <ul className="list-disc gap-2.5 flex flex-col my-0">
-                                <li className="list-disc text-lg font-bold text-[#2B3240]">
+                            <ul className="list-disc gap-2.5 flex flex-col m-0 p-0">
+                                <li className="list-disc text-lg font-normal text-[#2B3240]">
                                     <Trans i18nKey="Gambling Content" />
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="list-3 w-[750px] flex flex-col justify-center items-center gap-5">
-                        <div className="headerTitle-3  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl whitespace-nowrap">
-                            {t('Beware of beginner luck.')}
-                        </div>
+                    <div className="list-3 w-full md:w-[750px] flex flex-col justify-center items-center gap-5 mb-12">
+                        <div className="headerTitle-3  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl ">{t('Beware of beginner luck.')}</div>
                         <div className="contentList-3">
-                            <ul className="list-disc gap-2.5 flex flex-col my-0">
-                                <li className="list-disc text-lg font-bold text-[#2B3240]">
+                            <ul className="list-disc gap-2.5 flex flex-col m-0 p-0">
+                                <li className="list-disc text-lg font-normal text-[#2B3240]">
                                     <Trans i18nKey="Beware Content" />
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <img
-                        className="absolute top-28 right-14"
-                        src={card1}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-36 right-32"
-                        src={dice}
-                        alt=""
-                    />
                 </div>
             </div>
             <div className="responsibleSection-3 h-[600px]">
                 <div
-                    className="mainContainer  h-full w-full flex flex-col gap-5 justify-center items-center"
+                    className="mainContainer  h-full w-full flex flex-col gap-5 justify-center items-center bg-cover bg-no-repeat overflow-hidden"
                     style={{
                         backgroundImage: `url(${bgImg})`,
                     }}
                 >
-                    <img
-                        className="absolute top-14 left-32"
-                        src={cherry}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-24 left-36"
-                        src={crown}
-                        alt=""
-                    />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[21%] left-[11%] -ml-[75px] -mt-[70px]" src={cherry} />
+                    <img className="scale-50 md:scale-100 z-0 absolute top-[74%] left-[11%] -ml-[65px] -mt-[60px]" src={crown} />
+                    <img className="scale-50 md:scale-100 z-0 absolute bottom-0 right-0" src={girl2} />
 
-                    <div className="list-3 w-full flex flex-col justify-center items-center gap-5">
-                        <img src={usefulEn} alt="" />
-                        <div className="headerTitle-3  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl whitespace-nowrap">
-                            {t(
-                                'Before you gamble, here is some useful information for you.',
-                            )}
-                        </div>
+                    <div className="list-3 w-full flex flex-col justify-center items-center gap-5 mb-12">
+                        <img className="w-full md:w-[600px]" src={usefulEn} />
+                        <div className="headerTitle-3  bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl ">{t('Before you gamble, here is some useful information for you.')}</div>
                         <div className="contentList-3">
                             <div className="flex flex-col justify-center items-center gap-1 p-10 ">
-                                <span className="bg-gradient-to-b max-w-3xl from-[#673AB7] to-[#351A87] bg-clip-text text-transparent font-bold text-2xl">
+                                <span className="bg-gradient-to-b max-w-3xl from-[#673AB7] to-[#351A87] bg-clip-text text-transparent font-normal text-2xl">
                                     <Trans i18nKey="Before Content" />
                                 </span>
                             </div>
                         </div>
                     </div>
-                    <img
-                        className="absolute bottom-0 right-[-5px]"
-                        src={girl2}
-                        alt=""
-                    />
                 </div>
             </div>
             <div className="responsibleSection-4 h-[510px]">
                 <div className="mainContainer  h-full w-full flex flex-col gap-5 justify-center items-center bg-[#FAFAFA]">
-                    <img
-                        className="absolute bottom-40 left-36"
-                        src={chip}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-10 left-36"
-                        src={bubble}
-                        alt=""
-                    />
-                    <div className="list-4 w-full flex flex-col justify-center items-center gap-5">
-                        <img src={inquiriesEn} alt="" />
+                    <img className="absolute bottom-40 left-36" src={chip} />
+                    <img className="absolute bottom-10 left-36" src={bubble} />
+                    <div className="list-4 w-full flex flex-col justify-center items-center gap-5 mb-12">
+                        <img src={inquiriesEn} />
                         <div className="headerTitle-4 flex flex-col  text-center">
-                            <span className="bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl whitespace-nowrap ">
-                                {t(
-                                    'Normal response time (approximate): 72 hours',
-                                )}
-                            </span>
-                            <span className="bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl whitespace-nowrap">
-                                {t(
-                                    'Please contact us from your registered email address.',
-                                )}
-                            </span>
+                            <span className="bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl  ">{t('Normal response time (approximate): 72 hours')}</span>
+                            <span className="bg-gradient-to-b from-[#1EC3F9] to-[#2F80ED] bg-clip-text text-transparent font-bold text-3xl ">{t('Please contact us from your registered email address.')}</span>
                         </div>
                         <div className="contentList-1">
-                            <div className="flex flex-col justify-center items-center gap-5">
-                                <img className="" src={emailImg} alt="" />
-                                <button className="bg-[#4BA1FA] border-0 px-3 rounded-lg w-auto h-10 flex justify-center items-center text-white font-bold text-sm">
-                                    EMAIL: support@widusroyal.com
-                                </button>
+                            <div className="flex flex-col justify-center items-center gap-5 mb-12">
+                                <img className="" src={emailImg} />
+                                <button className="bg-[#4BA1FA] border-0 px-3 rounded-lg w-auto h-10 flex justify-center items-center text-white font-bold text-sm">EMAIL: support@widusroyal.com</button>
                             </div>
                         </div>
                     </div>
-                    <img
-                        className="absolute top-16 right-60"
-                        src={bubble}
-                        alt=""
-                    />
-                    <img
-                        className="absolute bottom-40 right-32"
-                        src={slotMachine}
-                        alt=""
-                    />
+                    <img className="absolute top-16 right-60" src={bubble} />
+                    <img className="absolute bottom-40 right-32" src={slotMachine} />
                 </div>
             </div>
         </>
