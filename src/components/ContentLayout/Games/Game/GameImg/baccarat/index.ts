71bc6429d3711cc24a96abd517453fd37916a503
Ren <s0985514623@gmail.com>
2023-09-11 00:58:11 +0800

    feat:EVO API 串接

---
+++ src/components/ContentLayout/Games/Game/GameImg/baccarat/index.ts
@@ -0,0 +1,8 @@
+import baccaratImg1 from '@/assets/images/fakeGamePic/exsamplePic (6).png';
+import baccaratImg2 from '@/assets/images/fakeGamePic/exsamplePic (7).png';
+import baccaratImg3 from '@/assets/images/fakeGamePic/exsamplePic (8).png';
+import baccaratImg4 from '@/assets/images/fakeGamePic/exsamplePic (14).png';
+import baccaratImg5 from '@/assets/images/fakeGamePic/exsamplePic (15).png';
+
+//包含圖片路徑的Array
+export const baccaratImg = [baccaratImg1, baccaratImg2, baccaratImg3, baccaratImg4, baccaratImg5];
--- src/components/ContentLayout/Games/Game/index.tsx
+++ src/components/ContentLayout/Games/Game/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
+import { getGameTypeImg } from '@/components/ContentLayout/Games/Game/GameImg/';
 import coinIcon from '@/assets/images/coin-icon.png';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-// import blackjackImg from '@/assets/images/blackjackImg.png';
-// import rouletteImg from '@/assets/images/rouletteImg.png';
-// import baccaratImg from '@/assets/images/baccaratImg.png';
-// import dragontigerImg from '@/assets/images/dragontigerImg.png';
+// import { getFakeImg } from '@/pages/Content/Taxonomy/Live/Evolution/fakeGameData';
 
 type GameProps = {
     data?: {
@@ -12,13 +10,21 @@ type GameProps = {
         'Table ID'?: string;
         'Direct Launch Table ID'?: string;
         'Game Type'?: string;
-        GameImg?: string;
+        'Bet Limit'?: {
+            KRW: {
+                symbol: string;
+                min: number;
+                max: number;
+            };
+        };
     };
+    imgiD?: number;
 };
 const index: React.FC<GameProps> = ({ data = {} }) => (
     <div className="gameWrap w-full px-1">
         <div className="gameImg w-full aspect-square relative align-top object-cover LazyLoadImage">
-            <LazyLoadImage src={data.GameImg} width="100%" height="100%" />
+            {/* <LazyLoadImage src={getFakeImg(imgiD + 1)} width="100%" height="100%" /> */}
+            <LazyLoadImage src={getGameTypeImg(data['Game Type'] as string)} width="100%" height="100%" />
             {/* <img className="w-full aspect-square align-top object-cover" src={data.GameImg} alt="" /> */}
         </div>
         <div className="gameInfo bg-[#363F4E] px-2 py-2">
@@ -28,7 +34,7 @@ const index: React.FC<GameProps> = ({ data = {} }) => (
         <div className="gameBalance flex justify-end gap-2 items-center bg-[#2B3240] px-2 py-1">
             <img className="h-[15px]" src={coinIcon} alt="" />
             <div className="balance">
-                <span className="text-white font-bold">¥ 100-50000</span>
+                <span className="text-white text-sm font-bold">{`${data['Bet Limit']?.KRW.symbol} ${data['Bet Limit']?.KRW.min} - ${data['Bet Limit']?.KRW.max}`}</span>
             </div>
         </div>
     </div>
