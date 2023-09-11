409fd5341516f1bad3d9abd3d2ec1be0e124d147
J7 <j7.dev.gg@outlook.com>
2023-09-11 21:42:52 +0800

    feat: 新增 ENV

--- src/utils/env.tsx
+++ src/utils/env.tsx
@@ -1,4 +1,4 @@
-export const APP_NAME = import.meta.env.APP_NAME || 'F22 FE';
+export const APP_NAME = import.meta.env.VITE_APP_NAME || 'F22 FE';
 export const APP_NAME_KEBAB = APP_NAME.toLowerCase().replace(/\s+/g, '-');
 export const APP_NAME_SNAKE = APP_NAME.toLowerCase().replace(/\s+/g, '_');
 export const API_URL = import.meta.env.VITE_API_URL || '/';
