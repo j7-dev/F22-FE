7a2db3475f8c32a0bbb0f7ae0935a5ab3dcf8b93
Ren <s0985514623@gmail.com>
2023-09-20 02:53:08 +0800

    fix:站內信加上點擊功能

--- src/components/ContentLayout/Header/NavBar/UserInfo/index.tsx
+++ src/components/ContentLayout/Header/NavBar/UserInfo/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSetAtom } from 'jotai';
 import { useUserAPI } from '@/hooks/useUserAPI';
 import { useTranslation } from 'react-i18next';
+import { selectedSectionAtom } from '@/pages/Content/Wallet';
 
 const index: React.FC = () => {
     const { t } = useTranslation();
     const { data, isLoading } = useUserAPI(); //TODO 同時還會在UserContainer中用到，怎麼讓他不要一直重複發送請求
     // console.log(data);
+    const setSelectedSection = useSetAtom(selectedSectionAtom);
     if (isLoading) return <div>loading...</div>;
     // const balance = data?.data.balances[0]?.amount || 0;
     const userName = data?.data?.username || 'userName';
@@ -17,7 +20,12 @@ const index: React.FC = () => {
                 {userName}
                 <span className={`${vip ? 'block' : 'hidden'} userVip text-sm font-bold text-[#f9a318] pl-2.5`}>{vip && vip}</span>
             </span>
-            <div className="h-10 gap-2.5  rounded-lg flex justify-between items-center px-2 hover:bg-[#F3F3F4] cursor-pointer">
+            <div
+                onClick={() => {
+                    setSelectedSection('siteMail');
+                }}
+                className="h-10 gap-2.5  rounded-lg flex justify-between items-center px-2 hover:bg-[#F3F3F4] cursor-pointer"
+            >
                 <div className="rewardInfo flex  justify-start items-center gap-2.5">
                     <span className="text-sm font-bold text-[#2B3240]">{t('站内信쪽지')}</span>
                 </div>
