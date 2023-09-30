e1f90a38b28092c31b5ed14ea7acf1dfbcc8ec70
Ren <s0985514623@gmail.com>
2023-09-30 10:56:05 +0800

    feat:1.SignUp增加隱藏Email欄位及Phone欄位

--- src/components/ContentLayout/Header/LoginPopUp/SignUp/index.tsx
+++ src/components/ContentLayout/Header/LoginPopUp/SignUp/index.tsx
@@ -1,5 +1,5 @@
 //1.API URL https://be-dev.smtbet7.com/api/auth/local/register
-//2.payload json 格式 帶入uuid(唯一值) email,username,password,confirmed": true,"blocked": false
+//2.payload json 格式 帶入uuid(唯一值) userEmail,username,password,confirmed": true,"blocked": false
 //3.不用帶入token
 import React, { useEffect, useState } from 'react';
 import { useSetAtom } from 'jotai';
@@ -8,13 +8,14 @@ import { useRegister } from '@refinedev/core';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 import { popupIsOpenAtom, loginOrSignUpAtom } from '@/components/ContentLayout/Header/LoginModule';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { AiOutlineMail } from 'react-icons/ai';
+import { AiFillPhone } from 'react-icons/ai';
 
 type LoginVariables = {
-    email: string;
+    userEmail: string;
     password: string;
     redirectPath: string;
-    username: string;
+    userName: string;
+    userPhone: number;
 };
 
 const index: React.FC = () => {
@@ -24,16 +25,16 @@ const index: React.FC = () => {
     const [form] = Form.useForm();
     const [verify, setVerify] = useState(false);
     const [verifyError, setVerifyError] = useState('');
-    const [submittable, setSubmittable] = useState(false);
-
-    const handleSignUp = (values: { email: string; userName: string; userPas: string }) => {
+    const [submitTable, setSubmitTable] = useState(false);
+    const handleSignUp = (values: { userEmail: string; userName: string; userPas: string; userPhone: number }) => {
         if (!verify) {
             setVerifyError('You must verify the captcha');
             return;
         }
-        const { email, userPas, userName } = values;
+        const { userEmail, userPas, userName, userPhone } = values;
+        // console.log('SignUpValues', values);
         register(
-            { email: email, username: userName, password: userPas, redirectPath: '/wallet' },
+            { userEmail: userEmail, userName: userName, password: userPas, userPhone: userPhone, redirectPath: '/wallet' },
             {
                 onSuccess: (data) => {
                     if (!data.success) {
@@ -63,35 +64,40 @@ const index: React.FC = () => {
 
     // Watch all values
     const values = Form.useWatch([], form);
-
+    // console.log('values', values);
     useEffect(() => {
+        //動態賦予userEmail
+        form.setFieldsValue({ userEmail: `${values?.userName}@smtbet7.com` });
         form.validateFields({ validateOnly: true }).then(
             () => {
-                setSubmittable(true);
+                setSubmitTable(true);
             },
             () => {
-                setSubmittable(false);
+                setSubmitTable(false);
             },
         );
     }, [values]);
+
     return (
         <div className="loginFrom text-center flex flex-col gap-2.5 w-full">
             <span>Available After SignUp.</span>
             {/* 錯誤訊息 */}
             {verifyError && <p className="text-danger text-red-600 font-bold">{verifyError}</p>}
             <Form form={form} onFinish={handleSignUp}>
-                <Form.Item name="email" hasFeedback rules={[{ required: true, message: 'Please input your Email' }]}>
-                    <Input addonBefore={<AiOutlineMail />} placeholder="User Email" />
-                </Form.Item>
+                <Form.Item hidden name="userEmail" />
+
                 <Form.Item name="userName" hasFeedback rules={[{ required: true, message: 'Please input your Name' }]}>
                     <Input addonBefore={<UserOutlined />} placeholder="User Name" />
                 </Form.Item>
                 <Form.Item name="userPas" hasFeedback rules={[{ required: true, message: 'Please input your Password' }]}>
                     <Input addonBefore={<LockOutlined />} placeholder="User Password" />
                 </Form.Item>
+                <Form.Item name="userPhone" hasFeedback rules={[{ required: true, message: 'Please input your Phone' }]}>
+                    <Input addonBefore={<AiFillPhone />} placeholder="User Phone" />
+                </Form.Item>
                 <HCaptcha sitekey="8a2b9bf5-aaeb-415f-b9a0-3243eefd798f" onVerify={() => handleVerificationSuccess()} />
                 <Form.Item>
-                    <Button disabled={!submittable} className="flex w-full h-10 items-center rounded-lg border-white gap-x-2 font-bold bg-[#F9A318] text-white hover:opacity-80  md:my-3 md:px-6 md:py-3 justify-center" htmlType="submit">
+                    <Button disabled={!submitTable} className="flex w-full h-10 items-center rounded-lg border-white gap-x-2 font-bold bg-[#F9A318] text-white hover:opacity-80  md:my-3 md:px-6 md:py-3 justify-center" htmlType="submit">
                         Sign Up
                     </Button>
                 </Form.Item>
