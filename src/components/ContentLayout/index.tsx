0e99735cc2e502e0a2153b83bfd3794e3613f3f1
Ren <s0985514623@gmail.com>
2023-10-09 12:48:16 +0800

    fix:解iphone 螢幕尺寸Bug

--- src/components/ContentLayout/index.tsx
+++ src/components/ContentLayout/index.tsx
@@ -39,10 +39,10 @@ const Layout = () => {
     }, [windowWidth]);
     return (
         <div className="contentLayout">
-            <div className={`leftContent z-50 overflow-hidden shadow-[2px_0px_20px_0px_rgba(163,112,237,0.25)] h-screen fixed left-0 top-0 xl:min-w-[88px] transition-all ${windowWidth <= 414 ? (mbSidebar ? 'w-60' : 'w-0') : ''}`}>
+            <div className={`leftContent z-50 overflow-hidden shadow-[2px_0px_20px_0px_rgba(163,112,237,0.25)] xl:min-w-[88px] h-screen fixed left-0 top-0  transition-all ${mbSidebar ? 'active' : ''}`}>
                 <Sidebar />
             </div>
-            <div className={`rightContent relative w-full bg-[#F8F9FF] ${windowWidth <= 414 ? (mbSidebar ? 'w-80' : 'w-0') : ''}`} onClick={handleCloseMbSidebar}>
+            <div className={`rightContent relative w-full bg-[#F8F9FF]`} onClick={handleCloseMbSidebar}>
                 <Header />
                 <Outlet />
                 <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
                 primary: '#1677ff',
             },
             screens: {
-                sm: '414px', // iphone SE
+                sm: '415px', // iphone SE
                 md: '810px', // ipad 直向
                 lg: '1080px', // ipad 橫向
                 xl: '1280px', // mac air
