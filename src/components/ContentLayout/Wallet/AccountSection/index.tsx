a1c4795c3ee901a75b8224070a57d38dcacd99e4
Ren <s0985514623@gmail.com>
2023-10-20 17:54:28 +0800

    fix:build error

--- src/components/ContentLayout/Wallet/AccountSection/index.tsx
+++ src/components/ContentLayout/Wallet/AccountSection/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import RolloverHistory from './RolloverHistory';
-// import CashHistory from './CashHistory';
+// import RolloverHistory from './RolloverHistory';
+// // import CashHistory from './CashHistory';
 import ChangePassword from './ChangePassword';
 
 interface AccountSectionProps {
     section?: string;
 }
 const index: React.FC<AccountSectionProps> = (props) => {
-    if (props.section === 'rolloverHistory') return <RolloverHistory />;
+    // if (props.section === 'rolloverHistory') return <RolloverHistory />;
     // if (props.section === 'cashHistory') return <CashHistory />;
     if (props.section === 'changePassword') return <ChangePassword />;
     return <></>;
