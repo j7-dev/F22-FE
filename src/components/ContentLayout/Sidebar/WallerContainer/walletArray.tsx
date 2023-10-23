022efb976f11d22cb89a0cfbaa446e69e310590e
Ren <s0985514623@gmail.com>
2023-10-24 00:17:16 +0800

    fix:rollingPoint

--- src/components/ContentLayout/Sidebar/WallerContainer/walletArray.tsx
+++ src/components/ContentLayout/Sidebar/WallerContainer/walletArray.tsx
@@ -5,7 +5,7 @@ export const walletArray = [
     { value: 'Withdraw', label: 'withdraw', showIn: 'afterLogin' },
     { value: 'Cash History', label: 'cashHistory', showIn: 'afterLogin' }, //存取紀錄
     //FIXME 多一個Rolling Point
-    // { value: 'rollingPoint', label: 'rollingPoint', showIn: 'afterLogin' },//洗码紀錄
+    { value: 'rollingPoint', label: 'rollingPoint', showIn: 'afterLogin' }, //洗码紀錄
     { value: 'Coupon History', label: 'couponHistory', showIn: 'afterLogin' }, //紅利紀錄
     { value: 'Notice', label: 'faq', showIn: 'beforeLogin' },
     { value: 'Sports Regulations', label: 'sportsRegulations', showIn: 'beforeLogin' },
