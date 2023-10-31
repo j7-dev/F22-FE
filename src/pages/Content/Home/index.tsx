4585c75c1f85a72165cdff82b54121896f2f03c0
Ren <s0985514623@gmail.com>
2023-10-31 10:55:54 +0800

    fix:Banner統一調整 #34

--- src/pages/Content/Home/index.tsx
+++ src/pages/Content/Home/index.tsx
@@ -7,27 +7,6 @@ import NewsMarquee from '@/components/ContentLayout/NewsMarquee';
 import { activeMenuAtom } from '@/components/ContentLayout/Sidebar';
 import { useGetMarketingContent } from '@/hooks/useGetMarketingContent';
 import { useShowPc } from '@/hooks/useShowPc';
-import banner_A from '@/assets/images/newBanner/banner_A.jpg';
-import banner_B from '@/assets/images/newBanner/banner_B.jpg';
-import banner_C from '@/assets/images/newBanner/banner_C.jpg';
-import banner_A_mobile from '@/assets/images/newBanner/banner_A_mobile.jpg';
-import banner_B_mobile from '@/assets/images/newBanner/banner_B_mobile.jpg';
-import banner_C_mobile from '@/assets/images/newBanner/banner_C_mobile.jpg';
-
-const bannerData = [
-    {
-        img: banner_A,
-        img_mobile: banner_A_mobile,
-    },
-    {
-        img: banner_B,
-        img_mobile: banner_B_mobile,
-    },
-    {
-        img: banner_C,
-        img_mobile: banner_C_mobile,
-    },
-];
 
 const Home = () => {
     const showPc = useShowPc();
@@ -42,7 +21,7 @@ const Home = () => {
     }, []);
     return (
         <div className="home sm:my-9 my-4 w-full flex flex-col sm:gap-8 gap-4">
-            <Banner bannerData={bannerData} />
+            <Banner />
             {!showPc ? <NewsMarquee speed={15} marqueeText={marqueeText} /> : ''}
             <PopularGames />
             {showPc ? <SlogGames /> : ''}
