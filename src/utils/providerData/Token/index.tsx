1a9b1647c974b529434a3cb9881bf0a1f7ef711d
J7 <j7.dev.gg@outlook.com>
2023-11-03 20:06:47 +0800

    feat: Token 小圖片

--- src/utils/providerData/Token/index.tsx
+++ src/utils/providerData/Token/index.tsx
@@ -1,14 +1,22 @@
 import { TGame } from '@/types/games';
-// import evoIcon from '@/assets/images/game_provider/evo_icon.png';
-// import ppIcon from '@/assets/images/game_provider/ppIcon.svg';
-import casinoIcon from '@/assets/images/Icon_Menu_Outlined_Casino.svg';
-import slotIcon from '@/assets/images/Icon_Menu_Outlined_Slot.svg';
-import gamesIcon from '@/assets/images/Icon_Menu_Outlined_Games.svg';
 import cq9 from '@/assets/images/game_provider/cq9.jpg';
 import wm from '@/assets/images/game_provider/wm.jpg';
 import aecasino from '@/assets/images/game_provider/aecasino.jpg';
 import agcasino from '@/assets/images/game_provider/agcasino.jpg';
 import mglive from '@/assets/images/game_provider/mglive.jpg';
+import roulette from '@/assets/images/game_provider/roulette.jpeg';
+import hilo from '@/assets/images/game_provider/hilo.jpeg';
+import hilo5s from '@/assets/images/game_provider/hilo5s.jpeg';
+import graph from '@/assets/images/game_provider/graph.jpeg';
+import baccarat from '@/assets/images/game_provider/baccarat.jpeg';
+import horse from '@/assets/images/game_provider/horse.jpeg';
+import fw from '@/assets/images/game_provider/fw.jpeg';
+import allbet from '@/assets/images/game_provider/allbet.png';
+import og from '@/assets/images/game_provider/og.png';
+import pt from '@/assets/images/game_provider/pt.png';
+import snail from '@/assets/images/game_provider/snail.jpg';
+import slot from '@/assets/images/game_provider/slot.jpg';
+import vsport from '@/assets/images/game_provider/vsport.jpeg';
 
 // 排除 Token 的 EVO & PP
 
@@ -17,7 +25,7 @@ export const tokenData: TGame[] = [
         gameName: '룰렛',
         gtype: 'roulette',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: roulette,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -25,7 +33,7 @@ export const tokenData: TGame[] = [
         gameName: '하이로우',
         gtype: 'hilo',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: hilo,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -33,7 +41,7 @@ export const tokenData: TGame[] = [
         gameName: '하이로우5초',
         gtype: 'hilo_5s',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: hilo5s,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -41,7 +49,7 @@ export const tokenData: TGame[] = [
         gameName: '그래프',
         gtype: 'graph',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: graph,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -49,7 +57,7 @@ export const tokenData: TGame[] = [
         gameName: '토큰바카라',
         gtype: 'baccarat',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: baccarat,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -57,7 +65,7 @@ export const tokenData: TGame[] = [
         gameName: '가상경마',
         gtype: 'horse',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: horse,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -65,7 +73,7 @@ export const tokenData: TGame[] = [
         gameName: '토큰휠',
         gtype: 'fw',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: fw,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -73,23 +81,23 @@ export const tokenData: TGame[] = [
         gameName: '토큰 달팽이레이싱',
         gtype: 'snail3 / snail4',
         openFn: 'iframe',
-        gameImg: gamesIcon,
+        gameImg: snail,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
-    {
-        gameName: '토큰 카지노',
-        gtype: 'tokencasino',
-        openFn: 'iframe',
-        gameImg: casinoIcon,
-        gameCategory: 'casino',
-        gameProviderName: 'token',
-    },
+    // { //未上市 不用接
+    //     gameName: '토큰 카지노',
+    //     gtype: 'tokencasino',
+    //     openFn: 'iframe',
+    //     gameImg: casinoIcon,
+    //     gameCategory: 'casino',
+    //     gameProviderName: 'token',
+    // },
     {
         gameName: '토큰슬롯',
         gtype: 'slot',
         openFn: 'blank',
-        gameImg: slotIcon,
+        gameImg: slot,
         gameCategory: 'slot',
         gameProviderName: 'token',
     },
@@ -97,7 +105,7 @@ export const tokenData: TGame[] = [
         gameName: '가상스포츠',
         gtype: 'vsports',
         openFn: 'blank',
-        gameImg: gamesIcon,
+        gameImg: vsport,
         gameCategory: 'games',
         gameProviderName: 'token',
     },
@@ -137,7 +145,7 @@ export const tokenData: TGame[] = [
         gameName: '플레이텍',
         gtype: 'pt',
         openFn: 'blank',
-        gameImg: casinoIcon,
+        gameImg: pt,
         gameCategory: 'casino',
         gameProviderName: 'token',
     },
@@ -153,7 +161,7 @@ export const tokenData: TGame[] = [
         gameName: '오리엔탈 게이밍',
         gtype: 'og',
         openFn: 'blank',
-        gameImg: casinoIcon,
+        gameImg: og,
         gameCategory: 'casino',
         gameProviderName: 'token',
     },
@@ -161,7 +169,7 @@ export const tokenData: TGame[] = [
         gameName: '올뱃',
         gtype: 'allbet',
         openFn: 'blank',
-        gameImg: casinoIcon,
+        gameImg: allbet,
         gameCategory: 'casino',
         gameProviderName: 'token',
     },
