28981643ba782fd2d0994b132b72cc3cab238b86
J7 <j7.dev.gg@outlook.com>
2023-11-19 09:21:33 +0800

    feat: hide me Error

--- src/providers/strapi-v4/utils/axios.ts
+++ src/providers/strapi-v4/utils/axios.ts
@@ -9,14 +9,16 @@ axiosInstance.interceptors.response.use(
     },
     (error) => {
         console.log('⭐  error:', error);
+        const requestUrl = error?.config?.url || '';
         const response = error?.response?.data?.error || 'Error';
         const message = typeof response === 'string' ? response : response?.message || JSON.stringify(response);
 
-        notification.error({
-            message,
-        });
-
-        return Promise.reject(error);
+        if (!requestUrl.toLowerCase().includes('/api/users/me')) {
+            notification.error({
+                message,
+            });
+            return Promise.reject(error);
+        }
     },
 );
 
