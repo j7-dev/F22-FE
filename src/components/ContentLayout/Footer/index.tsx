cb5651af7b7f3fdb8f72ba29dd0cf1912fc1f9aa
Ren <s0985514623@gmail.com>
2023-11-26 21:13:37 +0800

    fix:footer qrCode彈窗調整

--- src/components/ContentLayout/Footer/index.tsx
+++ src/components/ContentLayout/Footer/index.tsx
@@ -1,52 +1,64 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'antd';
+import { useModal } from '@refinedev/antd';
 // import { useSetAtom } from 'jotai';
 // import { Link } from 'react-router-dom';
 import LanguageSwitch from '@/components/ContentLayout/Header/LanguageSwitch';
 import { socialMedia } from '@/utils/menuData/socialMedia';
 import { gameCategories } from '@/utils/GameCategory';
-import QRCode from 'qrcode';
+// import QRCode from 'qrcode';
 import Icon_Main_Title from '@/assets/images/icon_main_title.svg';
 import { fakeProviderData } from '@/pages/Content/Home/Provider/ProviderData';
 import { useShowPc } from '@/hooks/useShowPc';
 import appStore from '@/assets/images/sideBar/appStore.png';
 import googlePlay from '@/assets/images/sideBar/googlePlay.png';
+import logo from '@/assets/images/1002_logo_f.svg';
+import logo2 from '@/assets/images/1002_logo_s.svg';
+import logo_footer from '@/assets/images/newMenuIcon/logo_icon+text.svg';
+import iosPopupImgPC from '@/assets/images/howToInstallApp/Smart_Bet_download_Web.jpg';
+import iosPopupImgMobile from '@/assets/images/howToInstallApp/Smart_Bet_download_Mobile.png';
+import androidPopupImgPC from '@/assets/images/howToInstallApp/Smart_Bet_download_Web_Android.jpg';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
+//TODO 很亂,有空需整理
 export const Footer: React.FC = () => {
     const showPc = useShowPc();
     const { t } = useTranslation();
+    const { show: showAndroid, modalProps: modalAndroid } = useModal();
+    const { show: showIos, modalProps: modalIos } = useModal();
     //QRCode
     const androidUrl = `https://${window.location.hostname}/apk/app.apk`;
     const iosUrl = `itms-services://?action=download-manifest&url=https://${window.location.hostname}/ipa/app.plist`;
-    const [androidQrCode, setAndroidQrCode] = React.useState('');
-    const [iosQrCode, setIosQrCode] = React.useState('');
-    const generateQR = async ({ setAndroidUrl, setIosUrl }: { setAndroidUrl: string; setIosUrl: string }) => {
-        try {
-            const androidResponse = await QRCode.toDataURL(setAndroidUrl, { margin: 0 });
-            const iosResponse = await QRCode.toDataURL(setIosUrl, { margin: 0 });
-            setAndroidQrCode(androidResponse);
-            setIosQrCode(iosResponse);
-        } catch (err) {
-            console.error(err);
-            return '';
-        }
-    };
-    useEffect(() => {
-        generateQR({ setAndroidUrl: androidUrl, setIosUrl: iosUrl });
-    }, []);
+    // const [androidQrCode, setAndroidQrCode] = React.useState('');
+    // const [iosQrCode, setIosQrCode] = React.useState('');
+    // const generateQR = async ({ setAndroidUrl, setIosUrl }: { setAndroidUrl: string; setIosUrl: string }) => {
+    //     try {
+    //         const androidResponse = await QRCode.toDataURL(setAndroidUrl, { margin: 0 });
+    //         const iosResponse = await QRCode.toDataURL(setIosUrl, { margin: 0 });
+    //         setAndroidQrCode(androidResponse);
+    //         setIosQrCode(iosResponse);
+    //     } catch (err) {
+    //         console.error(err);
+    //         return '';
+    //     }
+    // };
+    // useEffect(() => {
+    //     generateQR({ setAndroidUrl: androidUrl, setIosUrl: iosUrl });
+    // }, []);
     return (
         <div className="bg-white w-full text-black font-normal z-10 ">
-            <div className="footerImgWrap place-content-stretch grid md:grid-cols-11 md:pb-20 md:py-9 grid-cols-4 gap-4 w-full h-auto border-solid border-b-2 border-0 border-[#E0E0E0] py-6 px-4">
+            <div className="footerImgWrap place-content-stretch grid md:grid-flow-row md:grid-cols-11 md:pb-20 md:py-9 grid-cols-6 gap-4 w-full h-auto border-solid border-b-2 border-0 border-[#E0E0E0] py-6 px-4">
                 {/* Title */}
-                <div className="aboutTitle hidden md:flex md:flex-col md:col-start-2 md:col-span-3 gap-4 ">
-                    <div className=" md:h-10 col-span-1 h-full md:flex justify-start items-center text-center gap-3">
-                        <img src={Icon_Main_Title} alt="" />
-                        <span className="md:text-2xl text-[10px] font-bold text-black">{t('DOWNLOAD APP')}</span>
+                <div className="aboutTitle hidden md:flex justify-center md:justify-start md:flex-col md:col-start-2 md:col-span-3 gap-4 ">
+                    <div className="md:h-10 w-full col-span-1 h-full flex md:justify-start justify-center items-center text-center gap-3">
+                        <img src={showPc ? Icon_Main_Title : logo2} alt="" className="w-[30px]" />
+                        <span className="hidden md:block md:text-2xl text-[10px] font-bold text-black">{t('DOWNLOAD APP')}</span>
                     </div>
                 </div>
-                <div className="helpTitle w-full md:col-span-3 col-span-1 gap-4 grid  grid-cols-1">
+                <div className="helpTitle w-full md:col-span-3 col-span-1 gap-4 grid grid-cols-1">
                     <div className="md:text-2xl md:h-10 text-[10px] font-bold text-black flex items-center justify-start gap-3 col-span-1">
                         {showPc ? (
                             <>
@@ -54,7 +66,7 @@ export const Footer: React.FC = () => {
                                 <span className="">{t('SMART BET HELP')}</span>
                             </>
                         ) : (
-                            <span className="w-full h-full flex justify-center items-center">{t('SMRAT BET')}</span>
+                            <img src={logo_footer} alt="" className="w-full" />
                         )}
                     </div>
                 </div>
@@ -70,26 +82,42 @@ export const Footer: React.FC = () => {
                     </div>
                 </div>
                 {/* Content */}
-                <div className="aboutContent hidden md:flex md:flex-col md:col-start-2 md:col-span-3 gap-4">
+                <div className="aboutContent hidden md:flex md:flex-col md:col-start-2 col-span-3 gap-4 justify-between">
                     {/* qr code */}
-                    <div className="pr-10 md:grid grid-cols-2 col-span-3 gap-4">
-                        <div className="androidQrCode flex flex-col gap-4">
-                            <img src={androidQrCode} alt="" className="w-full" />
-                            <a href={androidUrl}>
+                    <div className="md:pr-10 grid grid-cols-2 col-span-3 gap-4">
+                        <div className="androidQrCode flex flex-col gap-4 justify-center">
+                            {/* <img src={androidQrCode} alt="" className="w-full" /> */}
+                            {/* TODO 未來看看有沒有辦法合併 */}
+                            <a href={androidUrl} className="md:hidden">
                                 <img src={googlePlay} alt="" className="w-full" />
                             </a>
+                            <a onClick={showAndroid} className="hidden md:block cursor-pointer">
+                                <img src={googlePlay} alt="" className="w-full" />
+                            </a>
+                            <Modal {...modalAndroid} classNames={{ mask: 'md:bg-[#000000d9] blur-sm', content: 'p-0 rounded-2xl overflow-hidden' }} closeIcon={<AiFillCloseCircle color="black" size={30} />} footer={null} centered>
+                                <img src={androidPopupImgPC} alt="" className="w-full" />
+                            </Modal>
                         </div>
-                        <div className="iosQrCode flex flex-col gap-4">
-                            <img src={iosQrCode} alt="" className="w-full" />
-                            <a href={iosUrl}>
+                        <div className="iosQrCode flex flex-col gap-4 justify-center ">
+                            {/* <img src={iosQrCode} alt="" className="w-full" /> */}
+                            <a onClick={showIos} className="cursor-pointer">
                                 <img src={appStore} alt="" className="w-full" />
                             </a>
+                            <Modal {...modalIos} className="w-auto" classNames={{ mask: 'md:bg-[#000000d9] blur-sm', content: 'bg-[#ebe3f9] p-0 rounded-2xl overflow-hidden' }} closeIcon={<AiFillCloseCircle color="black" size={30} />} footer={null} centered>
+                                <img src={showPc ? iosPopupImgPC : iosPopupImgMobile} alt="" className="w-full md:h-screen" />
+                                <a href={iosUrl} className="md:hidden justify-center flex mb-10">
+                                    <img src={appStore} alt="" className="w-1/2" />
+                                </a>
+                            </Modal>
                         </div>
+                    </div>
+                    <div className="hidden md:block footerLogo md:h-10 col-span-1 h-full text-center">
+                        <img src={logo} alt="" className="md:w-full w-[30px] h-full object-left object-contain" />
                     </div>
                     {/* 電腦版翻譯選單 */}
                     {showPc ? <LanguageSwitch /> : ''}
                 </div>
-                <div className="helpWrap w-full col-span-3 gap-4 grid grid-cols-1">
+                <div className="helpWrap w-full md:col-span-3 col-span-5 gap-4 grid grid-cols-1">
                     <ul className="socialMedia h-full md:grid-cols-4 gap-1.5 p-0 m-0 col-span-3 grid grid-cols-5 ">
                         {socialMedia.map((item) => {
                             return (
