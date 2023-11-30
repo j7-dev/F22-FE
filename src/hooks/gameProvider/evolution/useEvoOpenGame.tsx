2653197bb7caebef2fd70ff3b5dece5d96b7e6fa
Ren <s0985514623@gmail.com>
2023-11-30 11:32:00 +0800

    fix:config.channel

--- src/hooks/gameProvider/evolution/useEvoOpenGame.tsx
+++ src/hooks/gameProvider/evolution/useEvoOpenGame.tsx
@@ -35,9 +35,8 @@ export const useEvoOpenGame = () => {
                     url: `${API_URL}/api/evo/opengame`,
                     method: 'post',
                     values: {
-                        channel: {
-                            wrapped: false,
-                            mobile: false,
+                        config: {
+                            channel: { wrapped: false, mobile: false },
                         },
                     },
                 },
